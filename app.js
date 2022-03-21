a9efbcf04e5825a9f3b792f48c2354a72abbe386
Will Gundy <willgunderson503@gmail.com>
2022-03-21 15:57:10 -0700

    add search filter

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import { getDogs } from './fetch-utils.js';
 import { renderDogCard } from './render-utils.js';
 
 const dogListContainer = document.getElementById('dog-list-container');
+const filterEl = document.getElementById('filterDogsClient');
+
 
 // on load
 // fetch all dogs
@@ -16,3 +18,22 @@ window.addEventListener('load', async() => {
         dogListContainer.append(dogEl);
     }
 });
+
+filterEl.addEventListener('keyup', async() => {
+    const dogArray = await getDogs();
+
+    const input = filterEl.value.toUpperCase();
+
+    const filteredDogs = dogArray.filter(dog => dog.name.toUpperCase().indexOf(input) !== -1);
+
+    dogListContainer.innerHTML = '';
+
+    for (let dog of filteredDogs) {
+        const dogEl = renderDogCard(dog);
+
+        dogListContainer.append(dogEl);
+    }
+});
+
+
+
